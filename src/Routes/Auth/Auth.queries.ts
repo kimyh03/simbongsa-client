b954d305c20a910443674d2815451b40d1c800d8
Hoony <kimyh03@gmail.com>
2020-10-26 19:48:26 +0900

    Auth queries

--- src/Routes/Auth/Auth.queries.ts
+++ src/Routes/Auth/Auth.queries.ts
@@ -1,1 +1,23 @@
-export default () => "";
+import { gql } from "apollo-boost";
+
+export const SIGN_UP = gql`
+  mutation signUp($email: String!, $username: String!, $password: String!) {
+    signUp(args: { email: $email, username: $username, password: $password }) {
+      token
+    }
+  }
+`;
+
+export const SIGN_IN = gql`
+  mutation SignIn($email: String!, $password: String!) {
+    signIn(args: { email: $email, password: $password }) {
+      token
+    }
+  }
+`;
+
+export const LOCAL_SIGN_IN = gql`
+  mutation localSignIn($token: String!) {
+    localSignIn(token: $token) @client
+  }
+`;
