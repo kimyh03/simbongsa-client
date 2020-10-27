9cb5c2115d5df15a25ccceca1d1c784c85a1f9cc
Hoony <kimyh03@gmail.com>
2020-10-27 19:20:22 +0900

    Profile queries

--- src/Routes/Profile/Profile.queries.ts
+++ src/Routes/Profile/Profile.queries.ts
@@ -1,1 +1,68 @@
-export default () => "";
+import { gql } from "apollo-boost";
+
+export const LOCAL_LOG_OUT = gql`
+  mutation localSignOut {
+    localSignOut @client
+  }
+`;
+
+export const GET_PROFILE = gql`
+  query getProfile($userId: Float!) {
+    getProfile(args: { userId: $userId }) {
+      isSelf
+      user {
+        id
+        email
+        username
+        avatar
+        activityCount
+        activityTime
+        posts {
+          id
+          title
+          description
+          category
+          date
+          rigion
+          host
+          isOpened
+        }
+        certificates {
+          id
+          title
+          host
+          recognizedHours
+          date
+        }
+      }
+      applications {
+        id
+        status
+        post {
+          id
+          title
+          description
+          category
+          date
+          rigion
+          host
+          isOpened
+        }
+      }
+      likes {
+        id
+        createdAt
+        post {
+          id
+          title
+          description
+          category
+          date
+          rigion
+          host
+          isOpened
+        }
+      }
+    }
+  }
+`;
