8c6ea3f354dbf67c9d4743a5cbf6f8b337d52fc2
Hoony <kimyh03@gmail.com>
2020-10-27 15:48:09 +0900

    React helmet

--- src/Routes/Home/Home.presenter.tsx
+++ src/Routes/Home/Home.presenter.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import styled from "styled-components";
 
 const Container = styled.div``;
 
-export default () => <Container>Home</Container>;
+export default () => (
+  <Container>
+    <Helmet>
+      <title>Home | 자원봉사1365</title>
+    </Helmet>
+    Home
+  </Container>
+);
--- src/Routes/Profile/Profile.presenter.tsx
+++ src/Routes/Profile/Profile.presenter.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import styled from "styled-components";
 
 const Container = styled.div``;
 
-export default () => <Container>Profile</Container>;
+export default () => (
+  <Container>
+    <Helmet>
+      <title>Profile | 자원봉사1365</title>
+    </Helmet>
+    Profile
+  </Container>
+);
